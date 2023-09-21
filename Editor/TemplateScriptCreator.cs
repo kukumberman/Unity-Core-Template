c0fadeccadd8d6bd5befc174be2e6d42b9e23e77
Dima <53304126+kukumberman@users.noreply.github.com>
2023-09-21 15:17:33 +0300

    Update - script templates editor only

--- Editor/TemplateScriptCreator.cs
+++ Editor/TemplateScriptCreator.cs
@@ -4,7 +4,7 @@ using UnityEditor;
 public static class TemplateScriptCreator
 {
     private const string ScriptTemplateDirectory =
-        "Packages/Unity-Core-Template/Runtime/ScriptTemplates/";
+        "Packages/Unity-Core-Template/Editor/ScriptTemplates/";
     private const string Script_1 = "01-C# Script__Core__UI-MyHudView.cs.txt";
     private const string Script_2 = "02-C# Script__Core__UI Empty-MyHudView.cs.txt";
     private const string ItemDirectory = "Tools/ScriptTemplates/";
